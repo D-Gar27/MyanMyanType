b245716ad6863bf588917103049f8a8cbb7026c2
D-Gar27 <htetwinkhant999@gmail.com>
2022-02-12 10:49:29 +0630

    add leadrboard

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,4 +1,5 @@
 import { getProviders, signIn } from 'next-auth/react';
+import Head from 'next/head';
 import Link from 'next/link';
 import { FcGoogle } from 'react-icons/fc';
 import { useSelector } from 'react-redux';
@@ -20,21 +21,28 @@ interface State {
 const SignIn = ({ providers }: Props) => {
   const { theme } = useSelector((state: State) => state);
   return (
-    <main
-      className="w-full h-screen flex flex-col justify-center items-center gap-6"
-      style={{ backgroundColor: theme.dark }}
-    >
-      <button
-        className="text-xl flex items-center gap-4 border-2 rounded-sm border-current py-2 px-8"
-        style={{ color: theme.primary }}
-        onClick={() => signIn(providers.google.id, { redirect: true })}
+    <>
+      <Head>
+        <title>Sign In</title>
+      </Head>
+      <main
+        className="w-full h-screen flex flex-col justify-center items-center gap-6"
+        style={{ backgroundColor: theme.dark }}
       >
-        Sign in with <FcGoogle />
-      </button>
-      <Link href={'/'} passHref>
-        <button className="text-base text-white underline">back to home</button>
-      </Link>
-    </main>
+        <button
+          className="text-xl flex items-center gap-4 border-2 rounded-sm border-current py-2 px-8"
+          style={{ color: theme.primary }}
+          onClick={() => signIn(providers.google.id, { redirect: true })}
+        >
+          Sign in with <FcGoogle />
+        </button>
+        <Link href={'/'} passHref>
+          <button className="text-base text-white underline">
+            back to home
+          </button>
+        </Link>
+      </main>
+    </>
   );
 };
 
